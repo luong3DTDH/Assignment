0f548dc5734db6cfa8c9eba0c9ec593731a10d6d
luong3DTDH <65522901+luong3DTDH@users.noreply.github.com>
2020-06-22 17:06:11 +0700

    master

---
+++ product.js
@@ -0,0 +1,85 @@
+const express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+
+var url = 'mongodb+srv://luongdeptrai:12345@cluster0-geajk.mongodb.net/test';
+
+router.get('/', async (req, res) => {
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("Product"); 
+    let results = await dbo.collection("toy").find({}).toArray();
+    res.render('allproduct', { product: results });
+})
+
+///---------------------------Insert Toy---------------------------------------
+
+router.get('/insert', (req, res) => {
+    res.render('insert');
+})
+router.post('/insert', async (req, res) => {
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("Product");
+    let name = req.body.name;
+    let type = req.body.type;
+    let price = req.body.price;
+    let newproduct = {
+        name: name,
+        type: type,
+        price: price
+    };
+    await dbo.collection("toy").insertOne(newproduct);
+
+    let results = await dbo.collection("toy").find({}).toArray();
+    res.render('allproduct', { product: results });
+})
+
+
+/// --------------------------Edit Toy-----------------------------------------
+router.get('/edit', async(req,res)=>{
+    let id = req.query.id;
+    var ObjectID = require('mongodb').ObjectID;
+
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("Product");
+    let result = await dbo.collection("toy").findOne({"_id" : ObjectID(id)});
+    res.render('edit',{product:result});
+
+})
+///---------------------------Post edit infomation-----------------------------
+router.post('/edit', async(req,res)=>{
+    let id = req.body.id;
+    let name = req.body.name;
+    let type = req.body.type;
+    let price = req.body.price;
+    let newValues ={ name: name, type:type, price:price};
+    var ObjectID = require('mongodb').ObjectID;
+    let condition = {"_id" : ObjectID(id)};
+    
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("Product");
+    await dbo.collection("toy").updateOne(condition,newValues);
+    //
+    let results = await dbo.collection("toy").find({}).toArray();
+    res.render('allproduct',{product:results});
+})
+
+///------------------------------------Delete Toy------------------------------------
+
+
+router.get('/delete',async (req,res)=>{
+    let id = req.query.id;
+    var ObjectID = require('mongodb').ObjectID;
+    let condition = {"_id" : ObjectID(id)};
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("Product");
+    await dbo.collection("toy").deleteOne(condition);
+    //
+    let results = await dbo.collection("toy").find({}).toArray();
+    res.render('allproduct',{product:results});
+})
+
+
+
+
+module. exports = router;
